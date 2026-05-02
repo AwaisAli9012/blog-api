d587e1c55a6f0ac227512e22d850718f9fb67a9d
AwaisAli9012 <awaisaliimran8@gmail.com>
2026-05-02 00:38:20 -0700

    Add PUT route for updating posts

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,4 @@ use App\Http\Controllers\PostController;
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/{id}', [PostController::class, 'show']);
 Route::post('/posts', [PostController::class, 'store']);
+Route::put('/posts/{id}', [PostController::class, 'update']);
