0a81f16fdc847b05d1afbb7b2041d1d4d8c2fc97
AwaisAli9012 <awaisaliimran8@gmail.com>
2026-05-02 03:15:57 -0700

    Create Posts.vue page to list posts

---
+++ resources/js/Pages/Posts.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>All Posts</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        {{ post.title }} - {{ post.content }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+defineProps({ posts: Array });
+</script>
