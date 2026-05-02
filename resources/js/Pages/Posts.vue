1ccf4985d8a52941c9678fe36737295408d134ce
AwaisAli9012 <awaisaliimran8@gmail.com>
2026-05-02 07:06:24 -0700

    Update Posts.vue to display posts list via Inertia

--- resources/js/Pages/Posts.vue
+++ resources/js/Pages/Posts.vue
@@ -1,14 +1,16 @@
+<script setup>
+defineProps({
+  posts: Array
+})
+</script>
+
 <template>
   <div>
     <h1>All Posts</h1>
     <ul>
       <li v-for="post in posts" :key="post.id">
-        {{ post.title }} - {{ post.content }}
+        <strong>{{ post.title }}</strong> - {{ post.content }}
       </li>
     </ul>
   </div>
 </template>
-
-<script setup>
-defineProps({ posts: Array });
-</script>
