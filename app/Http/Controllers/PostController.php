d29efc67aa8c50c75dfc4b0580cb60583c21aefe
AwaisAli9012 <awaisaliimran8@gmail.com>
2026-05-01 23:46:23 -0700

    Add index method to PostController and fix show method to return JSON

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,10 +6,19 @@ use App\Models\Post; // we need the Post model
 
 class PostController extends Controller
 {
+
+public function index()
+{
+    // Get all posts
+    $posts=\App\Models\Post::all();
+
+    // Return them as JSON
+    return response()->json($posts);
+}
    public function show($id)
 {
     // Find the post by its ID
-   return $post = \App\Models\Post::findOrFail($id);
+   $post = \App\Models\Post::findOrFail($id);
     
 
     // Return it as JSON
