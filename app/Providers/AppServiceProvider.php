1e7f2d2d2aa1a9b14c2c1eaacfb9d563796b8f57
AwaisAli9012 <awaisaliimran8@gmail.com>
2026-05-03 08:20:46 -0700

    Simplify AppServiceProvider, fix session settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,17 +5,12 @@ use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
-    public function register(): void
-    {
-        //
-    }
+    public function register(): void {}
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
         if (app()->environment('production')) {
             URL::forceScheme('https');
-        $this->app['config']->set('session.secure', true);
-        $this->app['config']->set('session.same_site', 'none');
         }
     }
 }
