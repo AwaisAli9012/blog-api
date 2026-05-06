b96de2cabc0d78d131ff1bf964f9fb484c60beb2
AwaisAli9012 <awaisaliimran8@gmail.com>
2026-05-05 23:48:50 -0700

    Pass env vars to PHP-FPM with clear_env=no

--- docker/start.sh
+++ docker/start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-echo '[www]' > /usr/local/etc/php-fpm.d/zz-listen.conf
-echo 'listen = 127.0.0.1:9000' >> /usr/local/etc/php-fpm.d/zz-listen.conf
-php artisan config:clear 2>/dev/null || true
-php artisan cache:clear 2>/dev/null || true
+echo '[www]' > /usr/local/etc/php-fpm.d/zz-docker.conf
+echo 'listen = 127.0.0.1:9000' >> /usr/local/etc/php-fpm.d/zz-docker.conf
+echo 'clear_env = no' >> /usr/local/etc/php-fpm.d/zz-docker.conf
 php-fpm -D
 sleep 2
 nginx -g 'daemon off;'
