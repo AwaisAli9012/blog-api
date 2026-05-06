6d266ca940b926640c71770e528b7aa94f86b3fb
AwaisAli9012 <awaisaliimran8@gmail.com>
2026-05-05 23:48:24 -0700

    Clear config cache on startup

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo '[www]' > /usr/local/etc/php-fpm.d/zz-listen.conf
 echo 'listen = 127.0.0.1:9000' >> /usr/local/etc/php-fpm.d/zz-listen.conf
+php artisan config:clear 2>/dev/null || true
+php artisan cache:clear 2>/dev/null || true
 php-fpm -D
 sleep 2
 nginx -g 'daemon off;'
