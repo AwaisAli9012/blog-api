bd1e882a36ea6db6f551e2cf3cb26be05ee6d333
AwaisAli9012 <awaisaliimran8@gmail.com>
2026-05-06 00:03:20 -0700

    Run migrations on startup

--- docker/start.sh
+++ docker/start.sh
@@ -2,6 +2,7 @@
 echo '[www]' > /usr/local/etc/php-fpm.d/zz-docker.conf
 echo 'listen = 127.0.0.1:9000' >> /usr/local/etc/php-fpm.d/zz-docker.conf
 echo 'clear_env = no' >> /usr/local/etc/php-fpm.d/zz-docker.conf
+php artisan migrate --force
 php-fpm -D
 sleep 2
 nginx -g 'daemon off;'
