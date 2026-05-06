b95690687bd6772905ceb063ee786b6f70eeafe9
AwaisAli9012 <awaisaliimran8@gmail.com>
2026-05-05 23:54:21 -0700

    Write .env from environment variables at startup

--- docker/start.sh
+++ docker/start.sh
@@ -2,6 +2,21 @@
 echo '[www]' > /usr/local/etc/php-fpm.d/zz-docker.conf
 echo 'listen = 127.0.0.1:9000' >> /usr/local/etc/php-fpm.d/zz-docker.conf
 echo 'clear_env = no' >> /usr/local/etc/php-fpm.d/zz-docker.conf
+
+cat > /var/www/html/.env << ENVEOF
+APP_KEY=${APP_KEY}
+APP_ENV=${APP_ENV:-production}
+APP_DEBUG=${APP_DEBUG:-false}
+DB_CONNECTION=${DB_CONNECTION:-mysql}
+DB_HOST=${DB_HOST}
+DB_PORT=${DB_PORT:-3306}
+DB_DATABASE=${DB_DATABASE}
+DB_USERNAME=${DB_USERNAME}
+DB_PASSWORD=${DB_PASSWORD}
+SESSION_DRIVER=file
+CACHE_STORE=file
+ENVEOF
+
 php-fpm -D
 sleep 2
 nginx -g 'daemon off;'
