2ac361a95301f5d1881332a57cd3d53291625153
AwaisAli9012 <awaisaliimran8@gmail.com>
2026-05-12 03:42:50 -0700

    Add auto-post API endpoint for n8n

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,18 @@ Route::post('/posts', [PostController::class, 'store']);
 Route::put('/posts/{id}', [PostController::class, 'update']);
 Route::delete('/posts/{id}', [PostController::class, 'destroy']);Route::get('/posts', [App\Http\Controllers\PostController::class, 'apiIndex']);
 Route::get('/posts/{id}', [App\Http\Controllers\PostController::class, 'apiShow']);
+
+Route::post('/auto-post', function(Illuminate\Http\Request $request) {
+    $secret = $request->header('X-Secret-Key');
+    if ($secret !== env('AUTO_POST_SECRET')) {
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+    $user = App\Models\User::first();
+    $post = App\Models\Post::create([
+        'title' => $request->input('title'),
+        'content' => $request->input('content'),
+        'slug' => 'auto-' . time(),
+        'user_id' => $user->id,
+    ]);
+    return response()->json(['success' => true, 'post_id' => $post->id]);
+});
